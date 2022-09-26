5b4a2be99ee34d765e8b4f838f6c0363a9411439
Valentín Michilena <valentin.michilena@gmail.com>
2022-09-26 10:55:15 -0300

    BaSP-W06-2022: Added validation to First Name input.

--- Week-06/javascript/signup.js
+++ Week-06/javascript/signup.js
@@ -5,5 +5,31 @@ window.onload = function() {
 };
 function firstNameValidation() {
     var firstNameInput = document.getElementById("firstname-input");
-    console.log(firstNameInput);
+    var firstNameTry = firstNameInput.value;
+    var feedbackFirstName = document.getElementById("firstname-feedback");
+    var feedbackP = document.createElement("p");
+    if (firstNameTry.length == 0) {
+        return;
+    };
+    if (firstNameTry.length < 3) {
+        feedbackP.innerText = "It must contain at least 3 characters.";
+        feedbackP.classList.add("feedback-p-error");
+        feedbackFirstName.appendChild(feedbackP);
+        firstNameInput.classList.add("input-error");
+        return;
+    };
+    for (var i = 0; i < firstNameTry.length; i++) {
+        if (!isNaN(parseInt(firstNameTry[i])) || (firstNameTry[i].charCodeAt() > 32 && firstNameTry[i].charCodeAt() < 48)) {
+            feedbackP.innerText = "First name must contain only letters.";
+            feedbackP.classList.add("feedback-p-error");
+            feedbackFirstName.appendChild(feedbackP);
+            firstNameInput.classList.add("input-error");
+        };
+    };
+};
+function firstNameReset() {
+    var firstNameInput = document.getElementById("firstname-input");
+    var feedbackFirstName = document.getElementById("firstname-feedback");
+    feedbackFirstName.innerHTML = "";
+    firstNameInput.classList.remove("input-error");
 }
